7600533f6630156a54de0b82c1281a8574f2cc48
MagicianThirteen <MagicianThirteen@gmail.com>
2021-12-03 23:56:26 +0800

    定义物品类型，物品详情

--- FarmingRPG/Assets/Scripts/Enums/Enums.cs
+++ FarmingRPG/Assets/Scripts/Enums/Enums.cs
@@ -11,3 +11,20 @@ public enum Direction
     up,
     down
 }
+
+public enum ItemType
+{
+    Seed,
+    Commodity,
+    Watering_tool,
+    Hoeing_tool,//锄头
+    Chopping_tool,//砍
+    Breaking_tool,
+    Reaping_tool,//收割的工具
+    Collecting_tool,
+    Reapable_scenary,
+    none,
+    count
+}
+
+
