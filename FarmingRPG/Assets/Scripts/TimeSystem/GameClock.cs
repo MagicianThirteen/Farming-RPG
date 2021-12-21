22476fbe113e7790dc82de803f358f0b1780050b
MagicianThirteen <MagicianThirteen@gmail.com>
2021-12-21 21:06:10 +0800

    时间界面UI

---
+++ FarmingRPG/Assets/Scripts/TimeSystem/GameClock.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameClock : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI yearText;
+}
