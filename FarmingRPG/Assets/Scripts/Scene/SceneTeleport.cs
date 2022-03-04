0b20a5d2366a18b9a09b8773ad556d7cd335ae08
MagicianThirteen <MagicianThirteen@gmail.com>
2022-03-04 09:40:40 +0800

    场景切换功能，以及新地图完成

---
+++ FarmingRPG/Assets/Scripts/Scene/SceneTeleport.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class SceneTeleport : MonoBehaviour
+{
+    public SceneName sceneGoTo;
+    [SerializeField]private Vector3 scenePostion;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Player player = other.GetComponent<Player>();
+        if (player != null)
+        {
+            float x = Mathf.Approximately(scenePostion.x, 0) ? player.transform.position.x : scenePostion.x;
+            float y = Mathf.Approximately(scenePostion.y, 0) ? player.transform.position.y : scenePostion.y;
+            float z = 0;
+            scenePostion = new Vector3(x, y-4, z);
+            SceneControllerManager.Instance.FadeAndLoadScene(sceneGoTo.ToString(),scenePostion);
+
+        }
+        
+    }
+}
